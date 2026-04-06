b59515137183a89b23785f8682dd84b30e4969f6
ifthabeeb56-ui <ifthabeeb56@gmail.com>
2026-04-06 09:07:51 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,11 @@ import os
 PORTFOLIO_FILE = "portfolio_v7.csv"
 
 def load_data():
-    if os.path.exists(PORTFOLIO_FILE):
-        return pd.read_csv(PORTFOLIO_FILE)
-    # ഫയൽ ഇല്ലെങ്കിൽ വെറും ഒരു ലിസ്റ്റ് മാത്രം നൽകുന്നു
-    return pd.DataFrame(columns=["Category", "Name", "Buy Price", "QTY", "Investment", "Status"])
+    try:
+        if os.path.exists(PORTFOLIO_FILE):
+            return pd.read_csv(PORTFOLIO_FILE)
+        else:
+            # ഫയൽ ഇല്ലെങ്കിൽ പുതിയൊരു ബ്ലാങ്ക് ടേബിൾ റിട്ടേൺ ചെയ്യുന്നു
+            return pd.DataFrame(columns=["Category", "Name", "Buy Price", "QTY", "Investment", "Status"])
+    except:
+        return pd.DataFrame(columns=["Category", "Name", "Buy Price", "QTY", "Investment", "Status"])
