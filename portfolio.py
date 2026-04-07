f08419848d946e38c9e6167ab1677d241fabbd72
ifthabeeb56-ui <ifthabeeb56@gmail.com>
2026-04-07 04:37:34 +0300

    Create portfolio.py

---
+++ portfolio.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from utils import get_nifty500_tickers
+
+def add_stock_form():
+    with st.expander("➕ Add/Remove/Update Stock"):
+        all_stocks = get_nifty500_tickers()
+        
+        selected_stock = st.selectbox("Select Symbol from Nifty 500", all_stocks)
+        buy_price = st.number_input("Buy Price", min_value=0.0)
+        qty = st.number_input("Qty", min_value=1)
+        
+        if st.button("Add to Portfolio"):
+            st.success(f"{selected_stock} ആഡ് ചെയ്തിട്ടുണ്ട്!")
+          
